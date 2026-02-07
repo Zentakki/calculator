c51df30ea4093f1728c76e7111682d60419ce22e
Zentakki <likorkaffe@gmail.com>
2026-02-07 20:14:43 +0100

    finished extra credits, allow keyboard interaction and undo last input

--- script.js
+++ script.js
@@ -49,8 +49,6 @@ const clear = () => {
     operator = undefined;
     phase = 0;
     history = [];
-    currentInput = undefined;
-    lastInput = undefined;
     display.value = 0;
     dotBtn.disabled = false;
     buttons.forEach(button => button.classList.remove("selected"));
@@ -61,11 +59,11 @@ let number2 = "0";
 let operator;
 let phase = 0;
 let history = [];
-let currentInput = undefined;
-let lastInput = undefined;
 
+const html = document.querySelector("html");
 const container = document.querySelector(".container");
 const display = document.querySelector(".display");
+display.readOnly = true;
 const buttons = document.querySelectorAll("button");
 const dotBtn = document.querySelector(".dot-btn");
 container.addEventListener('click', (event) => {
@@ -78,80 +76,89 @@ container.addEventListener('click', (event) => {
     };
 
     if (phase === 0) {
+        if (inputStr === "🟰") return;
+
+        if (inputStr === "🔙" && number1 !== "0") {
+            number1 = history.pop();
+            display.value = number1;
+        }
         if (inputStr === "." && number1.includes(".")) {
-            // do nothing
+            // do nothing if . when already a decimal
             return
         };
         if (history[history.length - 2] === ".") {
             // do nothing, limit to only one decimal
             return
         };
-        if (inputStr === "." && (number1 === "0" || number1 === "-")) {
-            // this is to allow 0.X and -0.X
-            currentInput = inputStr;
-            number1 = "-" ? number1 = "-0." : "0.";
+        if (inputStr === "." && (number1 === "0")) {
+            // this is to allow 0.X
+            history.push(number1);
+            number1 = "0.";
             display.value = number1;
-            lastInput = inputStr;
-            history.push(lastInput);
-        } else if (inputStr === "➖" && lastInput === undefined) {
-            // input a negative number
-            currentInput = "-";
-            number1 = currentInput;
-            lastInput = inputStr;
-            history.push(lastInput);
+        } else if (inputStr === "+/-" && number1 !== "0") {
+            history.push(number1);
+            number1 = number1.split("").includes("-") ? String(Math.abs(Number(number1))) : `-${number1}`;
+            display.value = number1;
         } else if (Number(inputStr) || inputStr === "." || inputStr === "0") {
-            currentInput = inputStr;
-            number1 = number1 ? number1 += currentInput : currentInput;
+            history.push(number1);
+            number1 = number1 ? number1 += inputStr : inputStr;
             number1 = number1.replace(/^0+(?=\d)/, ""); // remove leading 0
             display.value = number1;
-            lastInput = inputStr;
-            history.push(lastInput);
         } else if (inputStr in operators) {
             buttons.forEach(button => button.classList.remove("selected"))
             operator = operators[inputStr];
             phase = 1;
             event.target.classList.add("selected");
-            lastInput = inputStr;
             history = [];
         };
     };
 
     if (phase === 1) {
+        if (inputStr === "🔙" && number2 !== "0") {
+            number2 = history.pop();
+            display.value = number2;
+        }
         if (inputStr === "." && number2.includes(".")) {
-            // do nothing
+            // do nothing if . when already a decimal
             return
         };
         if (history[history.length - 2] === ".") {
-            // do nothing, limit to only one decimal
+            // do nothing, limit to only one decimal place
             return
         };
 
-        if (inputStr === "." && (number2 === "0" || number2 === "-")) {
-            // this is to allow 0.X and -0.X
-            currentInput = inputStr;
-            number2 = "-" ? number2 = "-0." : "0.";
+        if (inputStr === "." && (number2 === "0")) {
+            // this is to allow 0.X
+            history.push(number2);
+            number2 = "0.";
             display.value = number2;
-            lastInput = inputStr;
-            history.push(lastInput);
-        } else if (inputStr === "➖" && lastInput === "➖" && number2 === "0") {
-            // input a negative number
-            currentInput = "-";
-            number2 = currentInput;
-            lastInput = inputStr;
-            history.push(lastInput);
+        } else if (inputStr === "+/-" && number2 !== "0") {
+            history.push(number2);
+            number2 = number2.split("").includes("-") ? String(Math.abs(Number(number2))) : `-${number2}`;
+            display.value = number2;
         } else if (inputStr in operators && number2 === "0") {
             buttons.forEach(button => button.classList.remove("selected"))
             operator = operators[inputStr];
             event.target.classList.add("selected");
-            lastInput = inputStr;
-            history.push(lastInput);
         } else if (Number(inputStr) || inputStr === "." || inputStr === "0") {
-            currentInput = inputStr;
-            number2 = number2 ? number2 += currentInput : currentInput;
+            history.push(number2);
+            number2 = number2 ? number2 += inputStr : inputStr;
             number2 = number2.replace(/^0+(?=\d)/, ""); // remove leading 0
             display.value = number2;
-            lastInput = inputStr;
-            history.push(lastInput);
+        } else if (inputStr in operators && inputStr !== "🟰") {
+            buttons.forEach(button => button.classList.remove("selected"))
+            event.target.classList.add("selected");
+            const result = operate(
+                operator,
+                Number(number1),
+                Number(number2)
+            );
+            display.value = result;
+            // allows to chain operations
+            number1 = String(result);
+            number2 = "0";
+            phase = 1;
+            operator = operators[inputStr];
         } else if (inputStr === "🟰") {
             buttons.forEach(button => button.classList.remove("selected"))
             const result = operate(
@@ -161,7 +168,7 @@ container.addEventListener('click', (event) => {
             );
             display.value = result;
             // allows to chain operations
-            number1 = result;
+            number1 = String(result);
             number2 = "0";
             operator = "";
             phase = 0;
@@ -172,6 +179,64 @@ container.addEventListener('click', (event) => {
     // console.log(`operator: ${operator}`);
     // console.log(`number2: ${number2}`);
     // console.log(`inputStr: ${inputStr}`);
-    // console.log(`lastInput: ${lastInput}`);
     // console.log(`history: ${history}`);
+});
+
+// keyboard interaction
+
+html.addEventListener('keypress', (event) => {
+    event.preventDefault();
+    switch (event.key) {
+        case "0":
+            document.querySelector("button.zero-btn").click();
+            break;
+        case "1":
+            document.querySelector("button.one").click();
+            break;
+        case "2":
+            document.querySelector("button.two").click();
+            break;
+        case "3":
+            document.querySelector("button.three").click();
+            break;
+        case "4":
+            document.querySelector("button.four").click();
+            break;
+        case "5":
+            document.querySelector("button.five").click();
+            break;
+        case "6":
+            document.querySelector("button.six").click();
+            break;
+        case "7":
+            document.querySelector("button.seven").click();
+            break;
+        case "8":
+            document.querySelector("button.eight").click();
+            break;
+        case "9":
+            document.querySelector("button.nine").click();
+            break;
+        case "-":
+            document.querySelector("button.subtract-btn").click();
+            break;
+        case "+":
+            document.querySelector("button.sum-btn").click();
+            break;
+        case "*":
+            document.querySelector("button.multiply-btn").click();
+            break;
+        case "/":
+            document.querySelector("button.divide-btn").click();
+            break;
+        case "Enter":
+            document.querySelector("button.equal-btn").click();
+            break;
+    };
 })
+
+html.addEventListener('keyup', (event) => {
+    event.preventDefault();
+    if (event.key === "Backspace") {
+        document.querySelector("button.back").click();
+    }});
